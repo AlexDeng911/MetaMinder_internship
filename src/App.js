19c268b6b2170d80496106453e35300fc4866169
galahad <galahad.serpstat@gmail.com>
2022-06-07 02:29:28 +0300

    Base config

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import './sass/app.scss';
+
+const App = () => {
+    return (
+        <div className="App">
+            Hello world!
+        </div>
+    );
+    
+}
+export default hot(module)(App);
